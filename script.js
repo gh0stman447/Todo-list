1b5c3f64b9cad53b1e705b1dcd986814df1510f7
gh0stman447 <o_malygin@bk.ru>
2022-07-03 00:31:19 +0300

    Modified delete button and add new task

--- script.js
+++ script.js
@@ -8,18 +8,33 @@ todoForm.addEventListener('submit', formHandler);
 function formHandler(event) {
     event.preventDefault();
     const taskText = todoInput.value;
+    
+    todoInput.focus();
+    if(taskText == '') return;
+
     const newTask = document.createElement('li');
+    style(newTask, {'position': 'relative', 'list-style': 'none'});
     newTask.innerText = taskText;
-    newTask.style['list-style-type'] = 'none';
-    const deleteButton = document.createElement('button');
-    deleteButton.setAttribute('role', 'button');
-    deleteButton.innerText = 'Удалить';
-    deleteButton.style['margin-left'] = '15px';
-    deleteButton.style['padding'] = '5px';
+
+    const deleteButton = document.createElement('img');
+    setAttribute(deleteButton, {'src': './img/krest.png', 'width': '30px', 'height': '30px'});
+    style(deleteButton, {right: '0', 'position': 'absolute'});
     newTask.append(deleteButton);
     
     deleteButton.addEventListener('click', (event) => event.target.closest('li').remove());
+
     todoList.append(newTask);
     todoInput.value = '';
-    todoInput.focus();
+}
+
+function setAttribute(el, attrs) {
+    for(let key in attrs) {
+        el.setAttribute(key, attrs[key]);
+    }
+}
+
+function style(el, styles) {
+    for(let key in styles) {
+        el.style[key] = styles[key];
+    }
 }
