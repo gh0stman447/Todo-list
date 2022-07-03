344ab541971e51598f2446c88ae5f55e45503b73
gh0stman447 <o_malygin@bk.ru>
2022-07-03 03:05:04 +0300

    Add complete task button and script

--- script.js
+++ script.js
@@ -13,28 +13,55 @@ function formHandler(event) {
     if(taskText == '') return;
 
     const newTask = document.createElement('li');
-    style(newTask, {'position': 'relative', 'list-style': 'none'});
-    newTask.innerText = taskText;
+    const wrapper = document.createElement('p');
+    
+    wrapper.className = 'text-task';
+    const completeTaskBtn = document.createElement('input');
+    newTask.className = 'task';
+    completeTaskBtn.className = 'complete-task-btn';
+    completeTaskBtn.type = 'checkbox';
+    newTask.append(completeTaskBtn);
+
+    wrapper.innerText = taskText;
+    newTask.insertAdjacentElement('beforeend', wrapper);
+
+
+    
 
     const deleteButton = document.createElement('img');
-    setAttribute(deleteButton, {'src': './img/krest.png', 'width': '30px', 'height': '30px'});
-    style(deleteButton, {right: '0', 'position': 'absolute'});
+    deleteButton.className = 'delete-btn';
+    deleteButton.setAttribute('src', './img/krest.png')
     newTask.append(deleteButton);
     
     deleteButton.addEventListener('click', (event) => event.target.closest('li').remove());
 
+    completeTaskBtn.addEventListener('click', function() {
+        const text = this.closest('li').querySelector('.text-task');
+        if (text.classList.contains('complete-task-style')) {
+            text.classList.remove('complete-task-style');
+        }
+        else {
+            text.classList.add('complete-task-style');
+        }
+        
+
+        
+
+    })
+
+    console.log(completeTaskBtn);
     todoList.append(newTask);
     todoInput.value = '';
 }
 
-function setAttribute(el, attrs) {
-    for(let key in attrs) {
-        el.setAttribute(key, attrs[key]);
-    }
-}
+// function setAttribute(el, attrs) {
+//     for(let key in attrs) {
+//         el.setAttribute(key, attrs[key]);
+//     }
+// }
 
-function style(el, styles) {
-    for(let key in styles) {
-        el.style[key] = styles[key];
-    }
-}
+// function style(el, styles) {
+//     for(let key in styles) {
+//         el.style[key] = styles[key];
+//     }
+// }
